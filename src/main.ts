54bc5ff958d231de2c4c443e924c0133c031adb5
Jakub Tyniecki <jakubtyniecki@gmail.com>
2017-02-10 00:16:14 +0000

    initial setup

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+/// <reference path="../typings/index.d.ts" />
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode } from '@angular/core';
+import { AppModule } from './app/app.module';
+if (process.env.ENV === 'production') {
+    enableProdMode();
+}
+platformBrowserDynamic().bootstrapModule(AppModule);
